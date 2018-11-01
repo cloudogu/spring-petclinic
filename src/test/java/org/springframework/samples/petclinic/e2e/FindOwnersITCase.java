a96b6382a5e40e9e3b483f52e812d133a7de5967
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2018-11-01 10:07:01 +0100

    Makes Selenium test executable with maven

--- src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java
+++ src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class FindOwnersTest {
+public class FindOwnersITCase {
 
     private static WebDriver driver;
 
