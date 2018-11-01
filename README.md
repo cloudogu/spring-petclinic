581e0c0702dc78bb46ff4bc5336687b417f74551
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2018-11-01 10:35:22 +0100

    Describes how to run selenium tests locally

--- README.md
+++ README.md
@@ -7,6 +7,32 @@ We extended it by
 
 * a [Smeagol Wiki](https://github.com/cloudogu/smeagol) (see [Home.md](docs/Home.md))
 * an [integration test](src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerITCase.java) (run with failsafe plugin, see [pom.xml](pom.xml))
-* a Jenkinsfile for building, testing and SonarQube analysis.
+* an [end to end test](src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java) using selenium
+* a [Jenkinsfile](Jenkinsfile) for building, testing and SonarQube analysis.
 
 For more details on petclinic, see also [original spring petclinci readme.md](readme-petclinic.md).
+
+
+## end 2 end tests
+
+The [existing e2e](src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java) is just a proof of 
+concept. It does ignore a lot of best practices, that should be taken into account in real projects 
+
+* It does not implement a page object
+* The selenium config (system properties) are included in the test, no separation of concerns.
+* It hard codes the `RemoteWebDriver`. For efficient local development you shoud provide multiple options such as local
+  (maybe headless) firefox or chrome, etc.
+* ...
+
+If you want to start them locally use a selenium grid such as [zalenium](https://github.com/zalando/zalenium)  
+```bash
+docker pull elgalu/selenium:3.14.0-p15
+docker run --rm -ti --name zalenium -p 4444:4444 \
+  -v /var/run/docker.sock:/var/run/docker.sock  -v /tmp/videos:/home/seluser/videos \
+ --privileged --network="host"  \
+ dosel/zalenium:3.14.0f start
+```
+* `--network="host"` is necessary so the grid can find the pet clinic that is started locally
+* See also [zalando/zalenium](https://github.com/zalando/zalenium)
+
+Then run: `mvn failsafe:integration-test failsafe:verify -Pe2e`
--- src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java
+++ src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java
@@ -3,21 +3,40 @@ package org.springframework.samples.petclinic.e2e;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FindOwnersITCase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FindOwnersITCase.class);
+
+    @Autowired
+    private Environment environment;
 
     private static WebDriver driver;
 
     @BeforeClass
     public static void startDriver() {
-        driver = Utils.getDriver();
+        driver = getDriver();
     }
 
     @Test
     public void addAndFindOwner() {
-        driver.get(Utils.getBaseUrl());
+        driver.get("http://localhost:" + environment.getProperty("local.server.port"));
 
         driver.findElement(By.cssSelector("[title^='find owners']")).click();
         driver.findElement(By.linkText("Add Owner")).click();
@@ -32,6 +51,19 @@ public class FindOwnersITCase {
         driver.findElement(By.cssSelector("[title^='find owners']")).click();
         driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Dent");
         driver.findElement(By.xpath("//button[contains(text(),'Find')]")).click();
+    }
+
+    private static WebDriver getDriver() {
+        String remoteUrl = System.getProperty("selenium.remote.url", "http://localhost:4444/wd/hub").toLowerCase();
+        String browser = System.getProperty(" selenium.browser", "chrome").toLowerCase();
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            return new RemoteWebDriver(new URL(remoteUrl), capabilities);
+        } catch (MalformedURLException e) {
+            LOG.error("Unable to create URL for selenium remote webdriver" + remoteUrl, e);
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterClass
