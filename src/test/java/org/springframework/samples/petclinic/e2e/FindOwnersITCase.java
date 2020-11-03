d0ed5cb78dbbcc9a68e37e88425c896f7b3ad36d
Christian Beyer <christian.beyer@cloudogu.com>
2020-11-03 14:18:57 +0100

    fix format issue

--- src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java
+++ src/test/java/org/springframework/samples/petclinic/e2e/FindOwnersITCase.java
@@ -22,70 +22,72 @@ import java.net.URL;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FindOwnersITCase {
 
-    private static final Logger LOG = LoggerFactory.getLogger(FindOwnersITCase.class);
+	private static final Logger LOG = LoggerFactory.getLogger(FindOwnersITCase.class);
 
-    @Autowired
-    private Environment environment;
+	@Autowired
+	private Environment environment;
 
-    private static WebDriver driver;
+	private static WebDriver driver;
 
-    @BeforeClass
-    public static void startDriver() {
-        driver = getDriver();
-    }
+	@BeforeClass
+	public static void startDriver() {
+		driver = getDriver();
+	}
 
-    @Test
-    public void addAndFindOwner() {
-        driver.get(getPetclinicUrl());
+	@Test
+	public void addAndFindOwner() {
+		driver.get(getPetclinicUrl());
 
-        driver.findElement(By.cssSelector("[title^='find owners']")).click();
-        driver.findElement(By.linkText("Add Owner")).click();
+		driver.findElement(By.cssSelector("[title^='find owners']")).click();
+		driver.findElement(By.linkText("Add Owner")).click();
 
-        driver.findElement(By.id("firstName")).sendKeys("Arthur");
-        driver.findElement(By.id("lastName")).sendKeys("Dent");
-        driver.findElement(By.id("address")).sendKeys("Planet");
-        driver.findElement(By.id("city")).sendKeys("Earth");
-        driver.findElement(By.id("telephone")).sendKeys("42");
-        driver.findElement(By.xpath("//button[contains(text(),'Add')]")).click();
+		driver.findElement(By.id("firstName")).sendKeys("Arthur");
+		driver.findElement(By.id("lastName")).sendKeys("Dent");
+		driver.findElement(By.id("address")).sendKeys("Planet");
+		driver.findElement(By.id("city")).sendKeys("Earth");
+		driver.findElement(By.id("telephone")).sendKeys("42");
+		driver.findElement(By.xpath("//button[contains(text(),'Add')]")).click();
 
-        driver.findElement(By.cssSelector("[title^='find owners']")).click();
-        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Dent");
-        driver.findElement(By.xpath("//button[contains(text(),'Find')]")).click();
-    }
+		driver.findElement(By.cssSelector("[title^='find owners']")).click();
+		driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Dent");
+		driver.findElement(By.xpath("//button[contains(text(),'Find')]")).click();
+	}
 
-    private static WebDriver getDriver() {
-        String remoteUrl = System.getProperty("selenium.remote.url", "http://localhost:4444/wd/hub").toLowerCase();
-        String browser = System.getProperty(" selenium.browser", "chrome").toLowerCase();
-        try {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setBrowserName(browser);
-            // This could be extended by adding e.g. the test name or success status that will then be reflected in the
-            // Video name: https://opensource.zalando.com/zalenium/#usage
-            return new RemoteWebDriver(new URL(remoteUrl), capabilities);
-        } catch (MalformedURLException e) {
-            LOG.error("Unable to create URL for selenium remote webdriver" + remoteUrl, e);
-            throw new RuntimeException(e);
-        }
-    }
+	private static WebDriver getDriver() {
+		String remoteUrl = System.getProperty("selenium.remote.url", "http://localhost:4444/wd/hub").toLowerCase();
+		String browser = System.getProperty(" selenium.browser", "chrome").toLowerCase();
+		try {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setBrowserName(browser);
+			// This could be extended by adding e.g. the test name or success status that
+			// will then be reflected in the
+			// Video name: https://opensource.zalando.com/zalenium/#usage
+			return new RemoteWebDriver(new URL(remoteUrl), capabilities);
+		}
+		catch (MalformedURLException e) {
+			LOG.error("Unable to create URL for selenium remote webdriver" + remoteUrl, e);
+			throw new RuntimeException(e);
+		}
+	}
 
-    private String getPetclinicUrl() {
-        // Allows faster local development or builds. Start PetClinic once, pass URL via the system property instead of
-        // Starting a new instance via SpringBootTest each time.
-        String petClinicUrl =
-            System.getProperty("selenium.petclinic.url",
-                               "http://" + getPetclinicHost() + ":" + environment.getProperty("local.server.port"));
-        System.out.println("PetClinicUrl=" + petClinicUrl);
-        return petClinicUrl;
-    }
+	private String getPetclinicUrl() {
+		// Allows faster local development or builds. Start PetClinic once, pass URL via
+		// the system property instead of
+		// Starting a new instance via SpringBootTest each time.
+		String petClinicUrl = System.getProperty("selenium.petclinic.url",
+				"http://" + getPetclinicHost() + ":" + environment.getProperty("local.server.port"));
+		System.out.println("PetClinicUrl=" + petClinicUrl);
+		return petClinicUrl;
+	}
 
-    private String getPetclinicHost() {
-        // When using selenium grid, this might be different to "localhost"
-        return System.getProperty("selenium.petclinic.host",
-                                  "localhost");
-    }
+	private String getPetclinicHost() {
+		// When using selenium grid, this might be different to "localhost"
+		return System.getProperty("selenium.petclinic.host", "localhost");
+	}
 
-    @AfterClass
-    public static void stopDriver() {
-        driver.quit();
-    }
+	@AfterClass
+	public static void stopDriver() {
+		driver.quit();
+	}
+
 }
